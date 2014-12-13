8a73e74d85fd6b54952d4a648bdc8d911f697d28
Burak BASTEM <bbastem@ku.edu.tr>
2014-12-13 12:02:23 +0200

    Created separate classes for windows on AplicationWindow class.

--- Chewy Lokum Legend/src/ApplicationWindow.java
+++ Chewy Lokum Legend/src/ApplicationWindow.java
@@ -4,6 +4,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,10 +16,10 @@ import javax.swing.border.EmptyBorder;
 public class ApplicationWindow extends JFrame {
 
 	private JPanel contentPane;
-	JPanel menuWindow;
-	JPanel levelSelectionWindow;
-	JPanel gameWindow;
-	JPanel pauseGameWindow;
+	MenuWindow menuWindow;
+	LevelSelectionWindow levelSelectionWindow;
+	GameWindow gameWindow;
+	PauseGameWindow pauseGameWindow;
 
 	public ApplicationWindow() {
 		super("Chewy Lokum Legend");
@@ -30,139 +31,75 @@ public class ApplicationWindow extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new CardLayout(0, 0));
 
-		// //////////////////////////////////////////////////////////////////////
-		menuWindow = new JPanel();
+		////////////////////////////////////////////////////////////////////////
+		menuWindow = new MenuWindow();
 		contentPane.add(menuWindow, "name_92135036815278");
 
-		menuWindow.setLayout(null);
-
-		JLabel gameTitleLabel = new JLabel("Chewy Lokum Legend");
-		gameTitleLabel.setForeground(Color.BLACK);
-		gameTitleLabel.setFont(new Font("Showcard Gothic", Font.PLAIN, 30));
-		gameTitleLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		gameTitleLabel.setBounds(140, 100, 360, 100);
-		menuWindow.add(gameTitleLabel);
-
-		JButton startNewGameButton = new JButton("Start New Game");
-		startNewGameButton.addActionListener(new ActionListener() {
+		menuWindow.startNewGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				menuWindow.setVisible(false);
 				levelSelectionWindow.setVisible(true);
 			}
 		});
-		startNewGameButton.setBounds(255, 230, 130, 35);
-		menuWindow.add(startNewGameButton);
 
-		JButton loadGameButton = new JButton("Load Game");
-		loadGameButton.addActionListener(new ActionListener() {
+		menuWindow.loadGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				menuWindow.setVisible(false);
 				gameWindow.setVisible(true);
 			}
 		});
-		loadGameButton.setBounds(255, 275, 130, 35);
-		menuWindow.add(loadGameButton);
 
-		JButton exitGameButton = new JButton("Exit Game");
-		exitGameButton.addActionListener(new ActionListener() {
+		menuWindow.exitGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
-		exitGameButton.setBounds(255, 320, 130, 35);
-		menuWindow.add(exitGameButton);
 
 		// ///////////////////////////////////////////////////////////////////////////
-		levelSelectionWindow = new JPanel();
+		levelSelectionWindow = new LevelSelectionWindow(2);
 		contentPane.add(levelSelectionWindow, "name_92176023342318");
 
-		levelSelectionWindow.setLayout(null);
-
-		JPanel level1Panel = new JPanel();
-		level1Panel.setBounds(200, 175, 100, 130);
-		level1Panel.setBackground(Color.BLACK);
-		levelSelectionWindow.add(level1Panel);
-		level1Panel.setLayout(null);
-
-		JButton level1Button = new JButton("Level 1");
-		level1Button.addActionListener(new ActionListener() {
+		((AbstractButton) levelSelectionWindow.levelPanels.get(0).getComponent(0)).addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				levelSelectionWindow.setVisible(false);
 				gameWindow.setVisible(true);
 			}
 		});
-		level1Button.setBounds(0, 100, 100, 30);
-		level1Panel.add(level1Button);
 
-		JPanel level2Panel = new JPanel();
-		level2Panel.setLayout(null);
-		level2Panel.setBounds(340, 175, 100, 130);
-		level2Panel.setBackground(Color.BLACK);
-		levelSelectionWindow.add(level2Panel);
-
-		JButton level2Button = new JButton("Level 2");
-		level2Button.addActionListener(new ActionListener() {
+		((AbstractButton) levelSelectionWindow.levelPanels.get(1).getComponent(0)).addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				levelSelectionWindow.setVisible(false);
 				gameWindow.setVisible(true);
 			}
-		});
-		level2Button.setBounds(0, 100, 100, 30);
-		level2Panel.add(level2Button);
+		});		
 
 		//////////////////////////////////////////////////////////////////////////////////
-		gameWindow = new JPanel();
+		gameWindow = new GameWindow();
 		contentPane.add(gameWindow, "name_92260090971391");
 
-		gameWindow.setLayout(null);
-
-		AnimationWindow animationWindow = new AnimationWindow(431, 431);
-		animationWindow.setLocation(0, 0);
-
-		gameWindow.add(animationWindow);
-		
-		InformationWindow informationWindow = new InformationWindow(180, 120);
-		informationWindow.setLocation(441, 0);
-		
-		
-		gameWindow.add(informationWindow);
-
-		JButton pauseGameButton = new JButton("Pause Game");
-		pauseGameButton.addActionListener(new ActionListener() {
+		gameWindow.pauseGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				gameWindow.setVisible(false);
 				pauseGameWindow.setVisible(true);
 			}
 		});
-		pauseGameButton.setBounds(461, 130, 120, 35);
-		gameWindow.add(pauseGameButton);
-		// ////////////////////////////////////////////////////////
-		pauseGameWindow = new JPanel();
-		contentPane.add(pauseGameWindow, "name_44933427952662");
-		pauseGameWindow.setLayout(null);
 
-		JButton resumeGameButton = new JButton("Resume Game");
-		resumeGameButton.addActionListener(new ActionListener() {
+		// ////////////////////////////////////////////////////////
+		pauseGameWindow = new PauseGameWindow();
+		contentPane.add(pauseGameWindow, "name_44933427952662");
+
+		pauseGameWindow.resumeGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				pauseGameWindow.setVisible(false);
 				gameWindow.setVisible(true);
 			}
 		});
-		resumeGameButton.setBounds(255, 175, 130, 35);
-		pauseGameWindow.add(resumeGameButton);
 
-		JButton saveGameButton = new JButton("Save Game");
-		saveGameButton.setBounds(255, 220, 130, 35);
-		pauseGameWindow.add(saveGameButton);
-
-		JButton quitGameButton = new JButton("Quit Game");
-		quitGameButton.addActionListener(new ActionListener() {
+		pauseGameWindow.quitGameButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				pauseGameWindow.setVisible(false);
 				menuWindow.setVisible(true);
 			}
 		});
-		quitGameButton.setBounds(255, 265, 130, 35);
-		pauseGameWindow.add(quitGameButton);
 	}
 }
---
+++ Chewy Lokum Legend/src/GameWindow.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+@SuppressWarnings("serial")
+public class GameWindow extends JPanel {
+
+	AnimationWindow animationWindow;
+	InformationWindow informationWindow;
+	JButton pauseGameButton;
+
+	public GameWindow() {
+		// TODO Auto-generated constructor stub
+		super();
+
+		setLayout(null);
+
+		animationWindow = new AnimationWindow(431, 431);
+		setLocation(0, 0);
+
+		add(animationWindow);
+
+		informationWindow = new InformationWindow(180, 120);
+		informationWindow.setLocation(441, 0);
+
+
+		add(informationWindow);
+
+		pauseGameButton = new JButton("Pause Game");
+
+		pauseGameButton.setBounds(461, 130, 120, 35);
+		add(pauseGameButton);
+	}
+
+}
