d5b1be0ace62f321228cc5e3b2765356ddfc0f7a
YusufErdal <yerdal@ku.edu.tr>
2014-12-14 13:50:22 +0200

    test

--- Chewy Lokum Legend/src/main/Application.java
+++ Chewy Lokum Legend/src/main/Application.java
@@ -2,6 +2,7 @@ package main;
 import ui.ApplicationWindow;
 
 public class Application {
+	//testing
 
 	public static void main(String[] args) {
 		ApplicationWindow frame = new ApplicationWindow();
